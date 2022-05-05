ef801f58573a22ae8d7573b1d8929343ef414baf
Arpit <arpit_jain@live.com>
2022-05-05 12:03:06 +0530

    feat: some javascript

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
 function toggleMenu(event) {
   console.log(event);
-  const menu = document.getElementsByClassName("main-navbar");
-  menu[0].classList.toggle("open");
+  const str = prompt('hello')
+  console.log(str, " from prompt")
+//   const menu = document.getElementsByClassName("main-navbar");
+//   menu[0].classList.toggle("open");
 }
 
 const btn = document.getElementById("menu-toggle");
