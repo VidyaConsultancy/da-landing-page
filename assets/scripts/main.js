f67d551b00db918c200a2acb15c94373bbba5cf2
Arpit <arpit_jain@live.com>
2022-05-05 11:50:19 +0530

    feat: responseive menu bar

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+function toggleMenu(event) {
+  console.log(event);
+  const menu = document.getElementsByClassName("main-navbar");
+  menu[0].classList.toggle("open");
+}
+
+const btn = document.getElementById("menu-toggle");
+btn.addEventListener("click", function (e) {
+  const menu = document.getElementsByClassName("main-navbar");
+  menu[0].classList.toggle("open");
+});
